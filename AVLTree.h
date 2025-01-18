5ad575169a5b1bd80dbd4b6e4833a5694d98ea87
William Zhang <wzhang20@yahoo.com>
2025-01-17 22:55:04 -0500

    fix avl bugs

--- AVLTree.h
+++ AVLTree.h
@@ -41,13 +41,35 @@ class AVLTree<T>::Node {
     T key;
     Node* left;
     Node* right;
+    Node* parent;
     short height;
     short balance_factor;
 public:
     Node& operator=(const Node& node) = delete;
-    Node&& operator=(node&& node) = delete; // protect against bad operations
+    Node&& operator=(Node&& node) = delete; // protect against bad operations
     int depth();
     int height();
     Node& predecessor();
     Node& successor();
 };
+
+template <typename T>
+void AVLTree<T>::RotateLeft(Node& x) {
+    Node* y = x.right;
+    x.right = y->right;
+    if(y->left != nullptr) {
+        y->left->parent = x;
+    }
+    y->parent = x.parent;
+    if(x.parent == nullptr) {
+        _root = y;
+    }
+    else if(x == x.parent->left) {
+        x.parent->left = y;
+    }
+    else {
+        x.parent->right = y;
+    }
+    y->left = x;
+    x->parent = y;
+}
