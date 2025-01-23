53032b61d5fa10dd1ada71efbc04cd2d1e640e52
William Zhang <wzhang20@yahoo.com>
2025-01-22 22:39:29 -0500

    add AVLTree rotations + constructors

--- AVLTree.h
+++ AVLTree.h
@@ -1,15 +1,16 @@
 #pragma once
 #include <iostream>
+#define TEMPLATE template <typename T> // custom macro!!!
 
 /*
 AVL Tree
 */
 
-template <typename T>
+TEMPLATE
 class AVLTree {
     class Node; // Nested class
-    int size;
-    Node* _root;
+    int size_;
+    Node* root_;
 public:
     AVLTree();
     AVLTree(const AVLTree& tree); // copy constructor
@@ -18,25 +19,39 @@ public:
 
     bool operator==(AVLTree& rhs);
 
+    void print() const;
+
     bool empty() const;
     int size() const;
-    T min();
-    T max();
+    T* min();
+    T* max();
     
     void push(T key);
     T* find(T key);
     void modifyKey(Node& node, T newVal);
     void erase(Node& curr);
-private:
-    void RotateLeft(Node& x);
-    void RotateRight(Node& x);
 };
+
+TEMPLATE
+AVLTree<T>::AVLTree() :size_(0), root_(nullptr) {}
+
+TEMPLATE
+AVLTree<T>::AVLTree(const AVLTree& tree) :size_(tree.size) {
+    if(tree.root_) {
+        root_ = new Node(*tree.root_);
+    }
+}
+
+TEMPLATE
+void AVLTree<T>::print() const {
+    
+}
 
 /*
 AVL Tree Node
 */
 
-template <typename T>
+TEMPLATE
 class AVLTree<T>::Node {
     T key;
     Node* left;
@@ -45,31 +60,72 @@ class AVLTree<T>::Node {
     short height;
     short balance_factor;
 public:
-    Node& operator=(const Node& node) = delete;
-    Node&& operator=(Node&& node) = delete; // protect against bad operations
+    Node(const T& key, Node* parent=nullptr); // default value of parent is nullptr (e.g. for root)
+    Node(const T&& key, Node* parent=nullptr); // rvalue ref constructor (interesting one...)
+    Node(const T& key);
+    ~Node();
+    Node(const Node& node);
+    Node(Node&& node) noexcept;
+
+    Node& operator=(const Node& node) = delete; // protect against copy assignment
+    Node&& operator=(Node&& node) = delete; // protect against move assignment
+
+    std::string print(const int depth, int& max_depth, int& max_breadth);
+
+    static T* Find(Node* curr, const T& key);
+    static const T* Find(const Node* curr, const T& key);
     int depth();
-    int height();
     Node& predecessor();
     Node& successor();
+private:
+    void RotateLeft(Node& x);
+    void RotateRight(Node& x);
 };
 
-template <typename T>
-void AVLTree<T>::RotateLeft(Node& x) {
+TEMPLATE
+AVLTree<T>::Node::Node(const T& key, Node* parent) : 
+    key(key), height(0), balance_factor(0), left(nullptr), right(nullptr), parent(parent) {}
+
+TEMPLATE
+AVLTree<T>::Node::Node(const T&& key, Node* parent) : 
+    key(std::move(key)), height(0), balance_factor(0), left(nullptr), right(nullptr), parent(parent) {}
+
+TEMPLATE
+std::string AVLTree<T>::Node::print(const int depth, int& max_depth, int& max_breadth) {
+    
+}
+
+TEMPLATE 
+void AVLTree<T>::Node::RotateRight(Node& x) { // x is bottom-most node whose subtrees are out of balance
+    Node* y = x.left;
+    x.left = y->right; 
+    if(y->right != nullptr) {
+        y->right->parent = x;
+    }
+    y->parent = x.parent; // move left node up
+    if(x == x.parent->right) {
+        x.parent->left = y;
+    } else {
+        x.parent->left = y;
+    }
+    y->right = x; // x is right child of y now
+    x.parent = y;
+}
+
+TEMPLATE
+void AVLTree<T>::Node::RotateLeft(Node& x) {
     Node* y = x.right;
-    x.right = y->right;
+    x.right = y->left; // turn y’s left subtree into x’s right subtree
     if(y->left != nullptr) {
         y->left->parent = x;
     }
     y->parent = x.parent;
-    if(x.parent == nullptr) {
-        _root = y;
-    }
-    else if(x == x.parent->left) {
+    if(x == x.parent->left) {
         x.parent->left = y;
     }
     else {
         x.parent->right = y;
     }
     y->left = x;
-    x->parent = y;
+    x.parent = y;
 }
---
+++ test_AVLTree.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "AVLTree.h"
+
+int main() {
+    AVLTree<int> tree;
+    return 0;
+}
