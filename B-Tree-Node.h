5b59e800ed25bd434132d1acae69d30d796fd1fa
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2025-03-31 15:22:17 -0400

    make btree ruleof5

--- B-Tree-Node.h
+++ B-Tree-Node.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <vector>
 
-// Forward declare BTree
+// Forward declare BTree for node access
 template <typename T>
 class BTree;
 
@@ -9,18 +9,52 @@ template <typename T>
 class BTreeNode {
     friend class BTree<T>;
 public:
+    // Rule of 5 class!
     BTreeNode() {
         n = 0;
         leaf = true;
     }
-    int n;
-    std::vector<T> keys;
-    std::vector<BTreeNode*> children;
-    bool leaf;
+    ~BTreeNode() {
+        for(size_t i=0; i<children.size(); i++) {
+            delete children[i]; // will recurse
+        }
+    }
+    BTreeNode(BTreeNode<T>& node) : n(node.n), leaf(node.leaf), keys(node.keys), children(node.children) {}
+    BTreeNode(BTreeNode<T>&& node) noexcept : n(node.n), leaf(node.leaf), keys(std::move(node.keys)), children(std::move(node.children)) {
+        // node - vector move defaults to std::vector move constructor     
+        std::cout << "move constructor called\n";   
+    }
+    BTreeNode& operator=(BTreeNode<T>& rhs) { // perform deep copy
+        if(this != &rhs) {
+            n = rhs.n;
+            leaf = rhs.leaf;
+            keys = rhs.keys;
+            children = rhs.children;
+        }
+        return *this;
+    }
+    BTreeNode& operator=(BTreeNode<T>&& rhs) noexcept {
+        std::cout << "move assignment called\n";
+        if (this != &rhs) {
+            n = rhs.n;
+            leaf = rhs.leaf;
+            keys = std::move(rhs.keys);
+            children = std::move(rhs.children);
+
+            rhs.n = 0;
+            rhs.leaf = true;
+        }
+        return *this;
+    }
+private:
+    size_t n; // # of keys stored in page
+    std::vector<T> keys; // the keys themselves
+    std::vector<BTreeNode*> children; // pointers to children of size n+1
+    bool leaf; // whether it is a leaf page
     void printKeys() {
-        for(int i=0; i<n; i++) {
+        for(size_t i=0; i<n; i++) {
             std::cout << keys[i] << " ";
         }
-        std::cout << "\t";
+        std::cout << "\t"; // end of page/node
     }
 };
